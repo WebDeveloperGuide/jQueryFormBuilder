5b5e8fc79a72d6471b6121d6d9939982b6bb9c45
WebDeveloperGuide <8149549+WebDeveloperGuide@users.noreply.github.com>
2020-06-15 21:04:19 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>jQuery FormBuilder Tutorial</title>
+	
+	<!-- JQuery Script-->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+
+  	<!-- Form Builder Script -->
+  	<script src="https://formbuilder.online/assets/js/form-builder.min.js"></script>
+
+  	<!-- Custom Script -->
+  	<script src="js/script.js"></script>
+
+  	<!-- Bootstrap CSS -->
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" type="text/css" />
+
+  	<!-- Custom CSS -->
+  	<link rel="stylesheet" href="css/style.css" type="text/css" />  	
+</head>
+<body>
+	<!-- Builder Wrap -->
+	<div class="build-wrap form-wrapper-div"></div>
+</body>
+</html>
